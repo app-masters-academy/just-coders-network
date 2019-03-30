b84821b3ac736de0619fb67f00e0fab9e4807f4d
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 18:07:10 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -19,11 +19,6 @@ class App extends Component {
                     user_image="https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1229%2Cy_0%2Cw_2712%2Ch_3616%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_1081879181.jpg"
                     created_at="Hoje às 17:33"
                     content="Melhor rede social de todas, estou muito feliz!"/>
-                <Post
-                    user_name="Linus Torvald"
-                    user_image="https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1229%2Cy_0%2Cw_2712%2Ch_3616%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_1081879181.jpg"
-                    created_at="Hoje às 17:33"
-                    content="Melhor rede social de todas, estou muito feliz!"/>
             </div>
         );
     }
