dbbc67a12e84825bbab6b1a9fe7f411eda2347c5
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 17:14:52 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -11,25 +11,7 @@ class App extends Component {
                 <Header />
                 <PostForm />
                 <Post />
-                <div>
-                    <p>James Galvein Olive</p>
-                    <p>
-                        Nulla vulputate cursus libero a aliquet. Nulla nec rhoncus nunc. Nulla at
-                        faucibus orci, ac egestas augue. Donec sed sodales ligula. Class aptent taciti sociosqu ad
-                        litora torquent per conubia nostra, per inceptos himenaeos. Morbi id egestas justo, egestas
-                        ultrices quam. Donec ultrices commodo luctus. Nulla eget quam ut felis hendrerit porttitor eget
-                        viverra sem.
-                    </p>
-                    <div>
-                        <p>Linus</p>
-                        <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et dolor ac
-                            blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
-                    </div>
-                    <input/>
-                    <button>
-                        Comentar
-                    </button>
-                </div>
+                <Post />
             </div>
         );
     }
