68c37253c76e68aca3b483ef9563c01515ceea6c
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 15:34:32 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
                     <h3 style={{color: 'blue'}}>
                         Postar agora
                     </h3>
-                    <input style={{}}/>
+                    <input style={{display: 'flex'}}/>
                     <button>
                         Enviar
                     </button>
