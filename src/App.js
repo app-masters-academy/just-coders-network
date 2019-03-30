b24bb5061e9c728a09a5f7cf2f5e421c5fa41a00
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 14:55:02 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Mudando o texto.
+            Mudando o texto
           </p>
           <a
             className="App-link"
