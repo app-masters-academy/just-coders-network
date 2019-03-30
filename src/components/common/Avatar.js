60c736a45e81164b38784fc05aaccf39332a48a9
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 17:53:22 -0300

    Changes

--- src/components/common/Avatar.js
+++ src/components/common/Avatar.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class Avatar extends Component {
     render(){
+        console.log('Avatar props', this.props);
         return (
             <img src={this.props.image} />
         )
