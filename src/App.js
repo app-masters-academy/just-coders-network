5a897cff87e64c61a3c9b301cfb87400f53374c5
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 15:07:40 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <h1>Just coders Network</h1>
-        </div>
-        <div>
-          <h3>Postar agora</h3>
-          <input />
-          <button />
-        </div>
-        <div>
-          <p>Linus Torvalds</p>
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur iaculis massa magna, eget elementum diam pharetra sed. Morbi sagittis malesuada nisl, non tristique erat viverra at. Proin pulvinar tempor justo et tincidunt. Sed id dui purus. Mauris varius sem ultrices risus feugiat molestie. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec a nisl hendrerit, rhoncus dui ut, bibendum diam. Nam ipsum lorem, egestas nec feugiat sed, sollicitudin ut nisl. Nulla vulputate cursus libero a aliquet. Nulla nec rhoncus nunc. Nulla at faucibus orci, ac egestas augue. Donec sed sodales ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi id egestas justo, egestas ultrices quam. Donec ultrices commodo luctus. Nulla eget quam ut felis hendrerit porttitor eget viverra sem.
-          </p>
-          <div>
-            <p>Karla</p>
-            <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et dolor ac blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
-          </div>
+    render() {
+        return (
             <div>
-                <p>Jonny</p>
-                <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et dolor ac blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
+                <div>
+                    <h1>Just coders Network</h1>
+                </div>
+                <div>
+                    <h3>Postar agora</h3>
+                    <input/>
+                    <button/>
+                </div>
+                <div>
+                    <p>Linus Torvalds</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur iaculis massa magna, eget
+                        elementum diam pharetra sed. Morbi sagittis malesuada nisl, non tristique erat viverra at. Proin
+                        pulvinar tempor justo et tincidunt. Sed id dui purus. Mauris varius sem ultrices risus feugiat
+                        molestie. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                        Donec a nisl hendrerit, rhoncus dui ut, bibendum diam. Nam ipsum lorem, egestas nec feugiat sed,
+                        sollicitudin ut nisl. Nulla vulputate cursus libero a aliquet. Nulla nec rhoncus nunc. Nulla at
+                        faucibus orci, ac egestas augue. Donec sed sodales ligula. Class aptent taciti sociosqu ad
+                        litora torquent per conubia nostra, per inceptos himenaeos. Morbi id egestas justo, egestas
+                        ultrices quam. Donec ultrices commodo luctus. Nulla eget quam ut felis hendrerit porttitor eget
+                        viverra sem.
+                    </p>
+                    <div>
+                        <p>Karla</p>
+                        <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et dolor ac
+                            blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
+                    </div>
+                    <div>
+                        <p>Jonny</p>
+                        <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et dolor ac
+                            blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
