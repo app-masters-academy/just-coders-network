2da0843f4e0d8821367af09fd93aff2897ca7a04
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 17:33:37 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
 import Header from './components/header/';
-import PostForm from'./components/post/PostForm';
+import PostForm from './components/post/PostForm';
 import Post from './components/post/Post';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Header />
-                <PostForm />
+                <Header/>
+                <PostForm/>
                 <Post
                     user_name="Baraky"
                     user_image="https://thenypost.files.wordpress.com/2018/05/180516-woman-mauled-by-angry-wiener-dogs-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1"
                     created_at="Ontem às 19:35"
                     content="Primeiro post nessa rede social muito legal"/>
-                <Post />
-                <Post />
+                <Post
+                    user_name="Linus Torvald"
+                    user_image="https://thenypost.files.wordpress.com/2018/05/180516-woman-mauled-by-angry-wiener-dogs-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1"
+                    created_at="Hoje às 17:33"
+                    content="Melhor rede social de todas, estou muito feliz!"/>
+                <Post/>
             </div>
         );
     }
