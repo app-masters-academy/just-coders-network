86d453154a8640088fa3811d6484a7a42ded0629
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 17:38:42 -0300

    Changes

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -4,7 +4,9 @@ import {styles, colors} from '../../styles/styles';
 const Post = (props) => {
     return (
         <div style={styles.grayBox}>
+            <img src={props.user_image}/>
             <p style={{color: colors.appBlue}}>{props.user_name}</p>
+
             <p style={{color: colors.appBlue}}>{props.created_at}</p>
             <div style={{...styles.box, backgroundColor: 'white'}}>
                 <p>{props.content}</p>
