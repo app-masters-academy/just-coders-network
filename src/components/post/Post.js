359474591eb120e8043d37ef0133a11a293766b5
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 17:37:22 -0300

    Changes

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -9,22 +9,22 @@ const Post = (props) => {
             <div style={{...styles.box, backgroundColor: 'white'}}>
                 <p>{props.content}</p>
             </div>
-            <div>
-                <p style={{color: colors.appBlue}}>Karla</p>
-                <div style={{...styles.box, backgroundColor: 'white', padding: 5}}>
-                    <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et
-                        dolor ac
-                        blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
-                </div>
-            </div>
-            <div>
-                <p style={{color: colors.appBlue}}>Jonny</p>
-                <div style={{...styles.box, backgroundColor: 'white', padding: 5}}>
-                    <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et
-                        dolor ac
-                        blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
-                </div>
-            </div>
+            {/*<div>*/}
+                {/*<p style={{color: colors.appBlue}}>Karla</p>*/}
+                {/*<div style={{...styles.box, backgroundColor: 'white', padding: 5}}>*/}
+                    {/*<p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et*/}
+                        {/*dolor ac*/}
+                        {/*blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>*/}
+                {/*</div>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+                {/*<p style={{color: colors.appBlue}}>Jonny</p>*/}
+                {/*<div style={{...styles.box, backgroundColor: 'white', padding: 5}}>*/}
+                    {/*<p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et*/}
+                        {/*dolor ac*/}
+                        {/*blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>*/}
+                {/*</div>*/}
+            {/*</div>*/}
             <input style={{width: '100%'}}/>
             <button
                 style={styles.blueButton}>
