2ba3e80aaab3b70dc26edeeef21a97a1e9ad1f5a
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 17:55:55 -0300

    Changes

--- src/components/common/Avatar.js
+++ src/components/common/Avatar.js
@@ -2,11 +2,16 @@ import React, {Component} from 'react';
 
 class Avatar extends Component {
     render(){
-        console.log('Avatar props', this.props);
         return (
-            <img src={this.props.image} />
+            <img styles={avatarStyles} src={this.props.image} />
         )
     }
+}
+
+const avatarStyles = {
+    width: 20,
+    height: 20,
+    borderRadius: 20
 }
 
 export default Avatar;
