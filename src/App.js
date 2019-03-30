4ac1e3bd926054b48bddc9bedd463146b61e50f4
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 15:11:23 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -43,6 +43,25 @@ class App extends Component {
                         Comentar
                     </button>
                 </div>
+                <div>
+                    <p>James Galvein Olive</p>
+                    <p>
+                        Nulla vulputate cursus libero a aliquet. Nulla nec rhoncus nunc. Nulla at
+                        faucibus orci, ac egestas augue. Donec sed sodales ligula. Class aptent taciti sociosqu ad
+                        litora torquent per conubia nostra, per inceptos himenaeos. Morbi id egestas justo, egestas
+                        ultrices quam. Donec ultrices commodo luctus. Nulla eget quam ut felis hendrerit porttitor eget
+                        viverra sem.
+                    </p>
+                    <div>
+                        <p>Linus</p>
+                        <p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et dolor ac
+                            blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>
+                    </div>
+                    <input/>
+                    <button>
+                        Comentar
+                    </button>
+                </div>
             </div>
         );
     }
