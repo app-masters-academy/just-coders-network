880916818e5b88d9754a55c1b18b210245459aff
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 15:03:05 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <div>
           <p>Linus Torvalds</p>
           <p>
-            
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur iaculis massa magna, eget elementum diam pharetra sed. Morbi sagittis malesuada nisl, non tristique erat viverra at. Proin pulvinar tempor justo et tincidunt. Sed id dui purus. Mauris varius sem ultrices risus feugiat molestie. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec a nisl hendrerit, rhoncus dui ut, bibendum diam. Nam ipsum lorem, egestas nec feugiat sed, sollicitudin ut nisl. Nulla vulputate cursus libero a aliquet. Nulla nec rhoncus nunc. Nulla at faucibus orci, ac egestas augue. Donec sed sodales ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi id egestas justo, egestas ultrices quam. Donec ultrices commodo luctus. Nulla eget quam ut felis hendrerit porttitor eget viverra sem.
           </p>
         </div>
       </div>
