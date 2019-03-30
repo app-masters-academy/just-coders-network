fecdd83f3aa37e4f8f81a2aac00529e2c50ac4c2
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 15:30:01 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <div className="Header" style={{color: 'green'}}>
+                <div className="Header">
                     <h1>Just coders Network</h1>
                 </div>
                 <div style={{textAlign: 'center'}}>
