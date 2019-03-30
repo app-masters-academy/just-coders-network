5e87dc1af48cf3c88b65c92444310fd8ebffd645
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 17:48:46 -0300

    Changes

--- src/components/common/Avatar.js
+++ src/components/common/Avatar.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Avatar extends Component {
     render(){
         return (
-            <img src="https://thenypost.files.wordpress.com/2018/05/180516-woman-mauled-by-angry-wiener-dogs-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1" />
+            <img src={this.props.image} />
         )
     }
 }
