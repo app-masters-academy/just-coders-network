4cc72a1ce2c9041fc055e8c5d57089b009e666e8
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 16:26:29 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Header from './components/Header';
+import Header from './components/Header.js';
 
 const appGray = '#EFEFEF';
 const appBlue = '#4990E2';
