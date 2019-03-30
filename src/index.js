1d6f64529bb27375e503d3d94f7aac0d1420ebcb
João Fernando Baraky <jfbaraky@gmail.com>
2019-03-30 15:20:20 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Appzinho from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Appzinho />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
