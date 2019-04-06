bde12ff3456051d1bbadcda1bb0c39e770e8cc34
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-06 15:44:39 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     componentDidMount() {
         console.log('App mountou!');
         const posts = getPosts();
+        this.setState({posts: posts});
     }
 
     render() {
