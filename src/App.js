467ff30818b0b1d346c086973c7a0a3dc8e50289
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-06 15:43:26 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
     }
 
     render() {
+        console.log('state do App: ', this.state);
         return (
             <div style={{padding: 20}}>
                 <Header/>
