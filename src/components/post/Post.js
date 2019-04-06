5ccf2e3fa44c0635af4f724b4161ee8e31b886c2
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-06 14:29:41 -0300

    Changes

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,42 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Avatar from '../common/Avatar';
 import {styles, colors} from '../../styles/styles';
 
-const Post = (props) => {
-    console.log('Post props', props);
-    return (
-        <div style={styles.grayBox}>
-            <Avatar
-                image={props.user_image}
-                name={props.user_name}
-                time={props.created_at}
-            />
-            <div style={{...styles.box, backgroundColor: 'white'}}>
-                <p>{props.content}</p>
-            </div>
-            {/*<div>*/}
+class Post extends Component {
+    render(){
+        return (
+            <div style={styles.grayBox}>
+                <Avatar
+                    image={props.user_image}
+                    name={props.user_name}
+                    time={props.created_at}
+                />
+                <div style={{...styles.box, backgroundColor: 'white'}}>
+                    <p>{props.content}</p>
+                </div>
+                {/*<div>*/}
                 {/*<p style={{color: colors.appBlue}}>Karla</p>*/}
                 {/*<div style={{...styles.box, backgroundColor: 'white', padding: 5}}>*/}
-                    {/*<p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et*/}
-                        {/*dolor ac*/}
-                        {/*blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>*/}
+                {/*<p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et*/}
+                {/*dolor ac*/}
+                {/*blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>*/}
                 {/*</div>*/}
-            {/*</div>*/}
-            {/*<div>*/}
+                {/*</div>*/}
+                {/*<div>*/}
                 {/*<p style={{color: colors.appBlue}}>Jonny</p>*/}
                 {/*<div style={{...styles.box, backgroundColor: 'white', padding: 5}}>*/}
-                    {/*<p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et*/}
-                        {/*dolor ac*/}
-                        {/*blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>*/}
+                {/*<p>Nulla facilisi. Curabitur tincidunt odio eu facilisis convallis. Maecenas commodo et*/}
+                {/*dolor ac*/}
+                {/*blandit. Proin tincidunt mauris odio, vel blandit mauris finibus sit amet.</p>*/}
                 {/*</div>*/}
-            {/*</div>*/}
-            <input style={{width: '100%'}}/>
-            <button
-                style={styles.blueButton}>
-                Comentar
-            </button>
-        </div>
-    )
+                {/*</div>*/}
+                <input style={{width: '100%'}}/>
+                <button
+                    style={styles.blueButton}>
+                    Comentar
+                </button>
+                <button
+                    onClick={() => {
+                        console.log('Clicou no botão');
+                        numLikes = numLikes + 1;
+                    }}
+                    style={{...styles.blueButton, marginLeft: 20}}>
+                    {'Likes ' + numLikes}
+                </button>
+            </div>
+        )
+    }
 }
 
 export default Post;
