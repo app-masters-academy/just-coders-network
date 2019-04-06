5f245a54683c918362bbfdaf29379e561cc43a81
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-06 16:59:14 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     addPostOnList(post){
         const posts = this.state.posts;
         posts.push(post);
+        this.setState({posts: posts});
     }
 
     // {condicao ? se true : se false}
