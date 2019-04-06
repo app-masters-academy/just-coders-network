daaddd9eda8bc64ee3f2aaefdc4c1ebfe813dd92
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-06 16:31:22 -0300

    Changes

--- src/components/post/PostForm.js
+++ src/components/post/PostForm.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {styles, appBlue} from '../../styles/styles'
 
-const PostForm = () => {
-    return (
-        <div style={styles.grayBox}>
-            <h3 style={{color: appBlue}}>
-                Postar agora
-            </h3>
-            <input style={{width: '100%'}}/>
-            <button
-                style={styles.blueButton}>
-                Enviar
-            </button>
-        </div>
-    )
-};
+class PostForm extends Component {
+    render(){
+        return (
+            <div style={styles.grayBox}>
+                <h3 style={{color: appBlue}}>
+                    Postar agora
+                </h3>
+                <input style={{width: '100%'}}/>
+                <button
+                    style={styles.blueButton}>
+                    Enviar
+                </button>
+            </div>
+        )
+    }
+}
 
 export default PostForm;
