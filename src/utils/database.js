97eb6a35a267e4ed42e7842765d1251a119df657
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-06 17:50:29 -0300

    Changes

--- src/utils/database.js
+++ src/utils/database.js
@@ -5,7 +5,7 @@ let fixedPost = [
         user_image: 'https://thenypost.files.wordpress.com/2018/05/180516-woman-mauled-by-angry-wiener-dogs-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1',
         created_at: 'Ontem às 19:35',
         content: 'Primeiro post nessa rede social muito legal'
-    },{
+    }, {
         id: 2,
         user_name: 'Linus Torvald',
         user_image: 'https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1229%2Cy_0%2Cw_2712%2Ch_3616%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_1081879181.jpg',
@@ -15,11 +15,11 @@ let fixedPost = [
 ];
 
 const getPosts = () => {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         let postArray = localStorage.getItem('posts');
-        if(postArray){
+        if (postArray) {
             postArray = JSON.parse(postArray);
-            setTimeout(()=> resolve(postArray), 2000);
+            setTimeout(() => resolve(postArray), 2000);
         } else {
             reject('Nenhum post salvo');
         }
@@ -28,20 +28,22 @@ const getPosts = () => {
 };
 
 const createPost = (post) => {
-    // Pegar meu array de posts
-    let postArray = localStorage.getItem('posts');
-    if(postArray){
-        postArray = JSON.parse(postArray);
-    } else {
-        postArray = [];
-    }
-    // Editar meu array de posts -> inserir o post novo
-    post.id = new Date().getTime();
-    post.created_at = 'Agora';
-    postArray.unshift(post);
-    // Salvar novamente meu array de posts
-    localStorage.setItem('posts', JSON.stringify(postArray));
-    return post;
+    return new Promise((resolve, reject) => {
+        // Pegar meu array de posts
+        let postArray = localStorage.getItem('posts');
+        if (postArray) {
+            postArray = JSON.parse(postArray);
+        } else {
+            postArray = [];
+        }
+        // Editar meu array de posts -> inserir o post novo
+        post.id = new Date().getTime();
+        post.created_at = 'Agora';
+        postArray.unshift(post);
+        // Salvar novamente meu array de posts
+        localStorage.setItem('posts', JSON.stringify(postArray));
+        setTimeout(() => resolve(post), 2000);
+    })
 };
 
 export {getPosts, createPost};
