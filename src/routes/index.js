cab5e3bd2f7fe8cf24f4faefff8dac5beb6e4465
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-13 14:45:17 -0300

    Changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 class Routes extends Component {
     render(){
         return(
-
+            <div>
+                
+            </div>
         )
     }
 }
