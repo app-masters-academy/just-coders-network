4cd6a92fb05673355d07fc93a839eeebe3f583d9
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-13 15:37:54 -0300

    Changes

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import {firebaseAuth, githubProvider} from '../../utils/firebase';
 import {signUp} from '../../utils/database';
 
