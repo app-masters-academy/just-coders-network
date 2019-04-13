7eaefafc8d424027363b188c405abd573942bf01
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-13 15:03:30 -0300

    Changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { Route, Router, Switch } from 'react-router';
-import { createBrowserHistory } from 'history';
+import React, {Component} from 'react';
+import {Route, Router, Switch} from 'react-router';
+import {createBrowserHistory} from 'history';
 import App from '../App';
 import SignUp from '../components/auth/SignUp';
 
 class Routes extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <Router history={createBrowserHistory()}>
                 <Switch>
-                <Route path='*' component={App}/>
-                <Route path='/entrar' component={SignUp}/>
+                    <Route path='*' component={App}/>
+                    <Route path='/entrar' component={SignUp}/>
                 </Switch>
             </Router>
         )
