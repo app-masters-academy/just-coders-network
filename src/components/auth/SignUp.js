07bfe40d2e2ba2128081b911accf26c2a5cb17ea
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-13 14:41:57 -0300

    Changes

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class SignUp extends Component {
     render(){
         return(
-            
+            <div>
+                <button>Entrar com Github</button>
+            </div>
         );
     }
 }
