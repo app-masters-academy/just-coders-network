0e8c126546cba7e5848c55906592d6bdcf3bac1e
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-13 14:50:13 -0300

    Changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
+import App from '../App';
+import SignUp from '../components/auth/SignUp';
 
 class Routes extends Component {
     render(){
         return(
             <div>
-                <Route path='/timeline'/>
+                <Route path='/timeline' component={App}/>
+                <Route path='/entrar' component={SignUp}/>
             </div>
         )
     }
