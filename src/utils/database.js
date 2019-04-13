8f6acdc123dbe9be12440b8266e77a39498e28ff
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-13 16:40:28 -0300

    Changes

--- src/utils/database.js
+++ src/utils/database.js
@@ -14,22 +14,7 @@ const getPosts = () => {
 };
 
 const createPost = (post) => {
-    return new Promise((resolve, reject) => {
-        // Pegar meu array de posts
-        let postArray = localStorage.getItem('posts');
-        if (postArray) {
-            postArray = JSON.parse(postArray);
-        } else {
-            postArray = [];
-        }
-        // Editar meu array de posts -> inserir o post novo
-        post.id = new Date().getTime();
-        post.created_at = 'Agora';
-        postArray.unshift(post);
-        // Salvar novamente meu array de posts
-        localStorage.setItem('posts', JSON.stringify(postArray));
-        setTimeout(() => resolve(post), 2000);
-    })
+
 };
 
 const baseUrl = 'http://api.justcoders.appmasters.io/dev';
