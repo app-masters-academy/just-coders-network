fcb41d2b2061abd1760aef97e68525150b8bfe8a
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-27 15:34:04 -0300

    Changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import {createBrowserHistory} from 'history';
 import App from '../App';
 import SignUp from '../components/auth/SignUp';
 import PostView from "../components/post/PostView";
+import Header from "../components/header";
 
 class Routes extends Component {
     constructor() {
@@ -23,6 +24,7 @@ class Routes extends Component {
         return (
             <Router history={createBrowserHistory()}>
                 <Switch>
+                    <Route path='*' component={Header}/>
                     {this.state.auth
                         ? <Switch>
                             <Route path='/post' component={PostView}/>
