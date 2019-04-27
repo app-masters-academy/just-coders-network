a6b593756af6ed4027a9701ff4d8d88b8360ad4f
João Fernando Baraky <jfbaraky@gmail.com>
2019-04-27 15:22:40 -0300

    Changes

---
+++ src/components/post/PostView.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PostView extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default PostView
